ec560953c8ae751039bffc369f53c22ec703f33e
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-29 16:14:41 +0530

    req routes

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require("http");
 
+const server = http.createServer((req, res) => {
+  //console.log("Amit");
+  //console.log(req.headers, req.method, req.url);
+  //process.exit()
+  const url=req.url;
+  if (url==="/"){
+    res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write('<body><form action="/message" method="POST"><input name="message"type="text"><button type="submit">Send</button></input></form></body>')
+  res.write("</html>");
+  return res.end()
+  }
 
-
-const server=http.createServer((req, res) => {
-  console.log("Amit");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write('<body><h1>Hi my name is Amit</h1></body>')
+  res.write("</html>");
+  res.end()
 });
 
-server.listen(4000)
+server.listen(3000);
