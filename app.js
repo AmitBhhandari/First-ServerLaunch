0b5c86afab5cf447bfdb6194c42c9468b6a777fe
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-30 14:22:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,9 @@
-const http = require("http");
+const http = require('http');
 
-const server = http.createServer((req, res) => {
-  //console.log("Amit");
-  //console.log(req.headers, req.method, req.url);
-  //process.exit()
-  const url = req.url;
-  if (url === "/Home") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h2>welcome to home</h2></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/about") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h2> Welcome to About Us page</h2></body>");
-    res.write("</html>");
-    return res.end();
-  } else if (url === "/node") {
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h2>Welcome to my Node Js project</h2></body>");
-    res.write("</html>");
-    return res.end();
-  }
+const routes = require('./routes');
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hi my name is Amit</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
